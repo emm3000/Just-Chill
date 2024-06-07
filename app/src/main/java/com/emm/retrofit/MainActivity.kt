3517e1e1f03c7d82854f4edf68a122c25b0b525a
Edgardo Muñoz <edgardo.emm20@gmail.com>
2024-06-07 11:07:23 -0500

    feat: Add edge to edge to main activity #15

--- app/src/main/java/com/emm/retrofit/MainActivity.kt
+++ app/src/main/java/com/emm/retrofit/MainActivity.kt
@@ -2,6 +2,9 @@ package com.emm.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 
@@ -11,7 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         navController = findNavController(R.id.nav_host_fragment)
     }
 
