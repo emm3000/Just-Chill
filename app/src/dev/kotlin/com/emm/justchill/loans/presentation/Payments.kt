bff871559a673212ebd87e3e480e2ff96216620a
Edgardo Muñoz <64887222+emm3000@users.noreply.github.com>
2024-07-27 01:02:22 -0500

    fix: fix edit from github app

--- app/src/dev/kotlin/com/emm/justchill/loans/presentation/Payments.kt
+++ app/src/dev/kotlin/com/emm/justchill/loans/presentation/Payments.kt
@@ -1,12 +1,24 @@
 package com.emm.justchill.loans.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.emm.justchill.core.theme.EmmTheme
 
 @Composable
 fun Payments(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp)
+            .padding(horizontal = 20.dp, vertical = 30.dp)
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
