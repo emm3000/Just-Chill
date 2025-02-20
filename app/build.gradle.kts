7c46f7ead8de7729dfb3dbe05dd6d49b0f12daa5
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-02-20 09:38:11 -0500

    feat: Update app version to 1.0.12-alpha, versionCode to 14
    - Increment `versionCode` from 13 to 14.
    - Update `versionName` from "1.0.11-alpha" to "1.0.12-alpha".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.11-alpha"
+        versionCode = 14
+        versionName = "1.0.12-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
