308e66bc0b1fd71809a7865312c66699ec02a3c6
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-02-28 17:04:42 -0500

    feat: Remove unused TransactionBackupRepository and SyncStatus
    - Remove `TransactionBackupRepository` interface.
    - Remove `SyncStatus` enum.

--- domain/src/main/java/com/emm/domain/transaction/SyncStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.emm.domain.transaction
-
-enum class SyncStatus {
-
-    PENDING_INSERT,
-    PENDING_UPDATE,
-    PENDING_DELETE,
-    SYNCED
-}
--- domain/src/main/java/com/emm/domain/transaction/TransactionBackupRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.emm.domain.transaction
-
-interface TransactionBackupRepository {
-
-    suspend fun seed()
-
-    suspend fun backup()
-}
