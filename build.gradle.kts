69231f9177b713bffc4de2c1757a2b93eb1aa178
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-02-28 16:45:06 -0500

    feat: Add `:data` module
    -   Add new module `:data` to manage data related logic.
    -   Add `proguard-rules.pro` file to manage code obfuscation in `:data` module.
    -   Add `ExampleInstrumentedTest.kt` for instrumented tests.
    -   Add `AndroidManifest.xml` file for `:data` module.
    -   Add `build.gradle.kts` file for `:data` module.
    -   Add `ExampleUnitTest.kt` for local unit tests.
    -   Add `.gitignore` for `:data` module.
    -   Add `consumer-rules.pro` file for `:data` module.
    -   Update `settings.gradle.kts` to include `:data` module.
    -   Update `libs.versions.toml` to include android library plugin.
    -   Update root `build.gradle.kts` to include android library plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.google.crashlytics) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.android.library) apply false
 }
