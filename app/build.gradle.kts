0c9f5ba3d5ff285f3f85190b2234487fcfc86b7e
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-03-11 17:48:49 -0500

    feat: Update app version to 0.0.18-alpha, versionCode to 17
    - Update `versionName` from 0.0.17-alpha to 0.0.18-alpha in `build.gradle.kts`.
    - Update `versionCode` to 17 in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.0.17-alpha"
+        versionCode = 17
+        versionName = "0.0.18-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
