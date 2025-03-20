e308c0d9de031f27cfc39e0a5ba49fc9fa152fc7
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-03-20 10:21:52 -0500

    feat: Update app version to 1.0.1-alpha, versionCode to 19
    - Update `versionName` from 1.0.0-alpha to 1.0.1-alpha in `build.gradle.kts`.
    - Update `versionCode` from 18 to 19 in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.0.0-alpha"
+        versionCode = 19
+        versionName = "1.0.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
