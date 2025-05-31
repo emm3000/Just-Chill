e764e3192a704c635ad697313298bbd751562de6
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-05-30 23:30:07 -0500

    feat: Update app version to 1.0.4-alpha, versionCode to 22
    - Update `versionName` from "1.0.3-alpha" to "1.0.4-alpha" in `build.gradle.kts`.
    - Update `versionCode` from 21 to 22 in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.0.3-alpha"
+        versionCode = 22
+        versionName = "1.0.4-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
