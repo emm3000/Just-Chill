de8604985cf529cb35b03867c508d2804b58179d
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-06-04 18:47:45 -0500

    feat: Update app version to 1.0.8-alpha, versionCode to 26
    - Update `versionName` from 1.0.7-alpha to 1.0.8-alpha in `build.gradle.kts`.
    - Update `versionCode` from 25 to 26 in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.0.7-alpha"
+        versionCode = 26
+        versionName = "1.0.8-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
