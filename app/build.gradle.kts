fd0fd8a0472a93611968ad860221e495f587b305
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-12-16 11:21:34 -0500

    feat: Update app version to 1.2.3-alpha
    - In `app/build.gradle.kts`, the `versionName` has been updated from "1.2.2-alpha" to "1.2.3-alpha".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.2.2-alpha"
+        versionCode = 31
+        versionName = "1.2.3-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
