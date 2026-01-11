b88ea7c97a8869dc4b5124685751e3c566481f02
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-01-11 13:41:21 -0500

    build: Update minimum SDK version
    - In `app/build.gradle.kts`, the `minSdk` is increased from 26 to 28.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.emm.justchill"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 36
         versionCode = 33
         versionName = "1.3.0-alpha"
