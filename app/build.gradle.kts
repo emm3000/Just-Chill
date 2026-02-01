968816a6f6908ae19446af58f2d1c742a68b69b7
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-02-01 11:27:05 -0500

    build: update versionCode and versionName in `build.gradle.kts`
    - Increment `versionCode` from 34 to 35.
    - Update `versionName` from "1.4.0-alpha" to "1.5.0-alpha".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.emm.justchill"
         minSdk = 28
         targetSdk = 36
-        versionCode = 34
-        versionName = "1.4.0-alpha"
+        versionCode = 35
+        versionName = "1.5.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
