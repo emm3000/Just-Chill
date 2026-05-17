d24456df92e9173f829afc65ccdec9221104cc0e
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-05-17 00:51:43 -0500

    fix(di): register Clock singleton for GetHomeDataUseCase
    factoryOf ignores constructor default values, so Koin failed to resolve
    the Clock parameter at instantiation time.

--- app/src/main/kotlin/com/emm/justchill/hh/di/dbModule.kt
+++ app/src/main/kotlin/com/emm/justchill/hh/di/dbModule.kt
@@ -4,6 +4,7 @@ import com.emm.data.provideDb
 import com.emm.data.provideSqlDriver
 import com.emm.data.provideTransactionQueries
 import com.emm.domain.home.GetHomeDataUseCase
+import kotlinx.datetime.Clock
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
@@ -14,5 +15,6 @@ val dbModule = module {
     single { provideDb(get()) }
     single { provideTransactionQueries(get()) }
 
+    single<Clock> { Clock.System }
     factoryOf(::GetHomeDataUseCase)
 }
