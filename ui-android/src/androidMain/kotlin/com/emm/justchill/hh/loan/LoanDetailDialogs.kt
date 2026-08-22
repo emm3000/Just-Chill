b9a734bea962fe59f313f30507c35c277edb6284
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-08-22 14:57:43 -0500

    test(loan): preview DeleteLoanDialog and DeleteLoanPaymentDialog

--- ui-android/src/androidMain/kotlin/com/emm/justchill/hh/loan/LoanDetailDialogs.kt
+++ ui-android/src/androidMain/kotlin/com/emm/justchill/hh/loan/LoanDetailDialogs.kt
@@ -4,6 +4,8 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.emm.justchill.core.theme.EmmTheme
 import com.emm.justchill.core.theme.LocalEmmColors
 
 @Composable
@@ -35,6 +37,31 @@ internal fun DeleteLoanDialog(
     )
 }
 
+private val previewLoanSummary = LoanSummaryUi(
+    personName = "Juan",
+    principal = "S/ 1,200.00",
+    interestPercentLabel = "5%",
+    totalDue = "S/ 1,260.00",
+    paidSoFar = "S/ 300.00",
+    remaining = "S/ 960.00",
+    isSettled = false,
+    readableLentAt = "12 de agosto de 2026",
+    note = "Para el arreglo del carro",
+)
+
+@Preview
+@Composable
+private fun DeleteLoanDialogPreview() {
+    EmmTheme {
+        DeleteLoanDialog(
+            summary = previewLoanSummary,
+            isDeleting = false,
+            onConfirm = {},
+            onDismiss = {},
+        )
+    }
+}
+
 @Composable
 internal fun DeleteLoanPaymentDialog(
     payment: LoanPaymentRowUi?,
@@ -62,4 +89,25 @@ internal fun DeleteLoanPaymentDialog(
             TextButton(onClick = onDismiss) { Text("Cancelar") }
         },
     )
+}
+
+private val previewLoanPayment = LoanPaymentRowUi(
+    paymentId = "1",
+    amount = "S/ 300.00",
+    methodLabel = "Yape",
+    readablePaidAt = "15 de agosto de 2026",
+    note = "Primer abono",
+)
+
+@Preview
+@Composable
+private fun DeleteLoanPaymentDialogPreview() {
+    EmmTheme {
+        DeleteLoanPaymentDialog(
+            payment = previewLoanPayment,
+            isDeleting = false,
+            onConfirm = {},
+            onDismiss = {},
+        )
+    }
 }
