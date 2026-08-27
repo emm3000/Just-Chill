8445b607ef61cda9c61958d1af8215bcf2910368
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-08-26 23:46:40 -0500

    chore(ios): delete the iosMain source sets

--- data/src/iosMain/kotlin/com/emm/data/shared/Dispatchers.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.emm.data.shared
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-
-/**
- * Kotlin/Native has no Dispatchers.IO; Default is the substitute.
- */
-actual val ioDispatcher: CoroutineDispatcher = Dispatchers.Default
